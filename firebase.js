e6f7130dba20b092014915df4d765b337a841d9b
SaadhviSree <129571759+SaadhviSree@users.noreply.github.com>
2024-07-01 13:59:27 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getDatabase, ref, get, child } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKzO1pAQ7P0DsZWmMpreZ7UrAyi1VgYV4",
-  authDomain: "mycalendar-tringapps.firebaseapp.com",
-  databaseURL:"https://mycalendar-tringapps-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  projectId: "mycalendar-tringapps",
-  storageBucket: "mycalendar-tringapps.appspot.com",
-  messagingSenderId: "654880458142",
-  appId: "1:654880458142:web:7c667ae4c715487f98428f"
+  apiKey: "API KEY",
+  authDomain: "AUTH DOMAIN LINK",
+  databaseURL:"DATABASE URL",
+  projectId: "PROJECT ID",
+  storageBucket: "STORAGE BUCKET LINK",
+  messagingSenderId: "MESSAGING SENDER ID",
+  appId: "APP ID"
 }
 
 const app = initializeApp(firebaseConfig)
